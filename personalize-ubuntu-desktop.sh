b2820e843dfe777dc15d24f49cdd5d3ade3cc13b
Robert <29333852+robertlarocca@users.noreply.github.com>
2021-05-06 23:13:25 -0400

    Format code with Prettier+

--- personalize-ubuntu-desktop.sh
+++ personalize-ubuntu-desktop.sh
@@ -2,8 +2,8 @@
 
 # Copyright (c) 2021 LaRoccx LLC <http://www.laroccx.com>
 
-script_version='2.6.12'
-script_release='devel'	# options devel, beta, release
+script_version='2.6.13'
+script_release='devel' # options devel, beta, release
 
 # Require root privileges to execute this script
 require_root_privileges() {
@@ -11,7 +11,7 @@ require_root_privileges() {
 		echo "Error: $(basename -s .sh $0) must be run as root!" 1>&2
 		exit 1
 	fi
-};
+}
 
 install_packages() {
 	echo 'Managing packages...'
@@ -30,7 +30,7 @@ install_packages() {
 	snap refresh
 	fwupdmgr --force refresh
 	fwupdmgr update
-};
+}
 
 hide_applications() {
 	echo 'Hiding applications...'
@@ -51,22 +51,22 @@ hide_applications() {
 		/usr/share/applications/vim.desktop \
 		/usr/share/applications/vmware-netcfg.desktop \
 		/usr/share/applications/vmware-player.desktop \
-		/var/lib/snapd/desktop/applications/powershell_powershell.desktop
+		/var/lib/snapd/desktop/applications/powershell_powershell.desktop;
 	do
-		if [[ "$(grep --count NoDisplay=true $desktop_file 2> /dev/null)" == "0" ]]; then
-			echo 'NoDisplay=true' >> $desktop_file
+		if [[ "$(grep --count NoDisplay=true $desktop_file 2>/dev/null)" == "0" ]]; then
+			echo 'NoDisplay=true' >>$desktop_file
 			echo "$(basename $desktop_file) is now hidden."
 		fi
 	done
-};
+}
 
 rename_applications() {
 	echo 'Renaming applications...'
 	sed -E -i s/'Name=AisleRiot Solitaire'/'Name=Solitaire'/g /usr/share/applications/sol.desktop
 	sed -E -i s/'Name=Cheese'/'Name=Webcam'/g /usr/share/applications/org.gnome.Cheese.desktop
 	sed -E -i s/'Name=Chromium Web Browser'/'Name=Chromium'/g /var/lib/snapd/desktop/applications/chromium_chromium.desktop
-	sed -E -i s/'Name=Disks'/'Name=Disk Utility'/g /usr/share/applications/org.gnome.DiskUtility.desktop
 	sed -E -i s/'Name=Disk Usage Analyzer'/'Name=Disk Usage'/g /usr/share/applications/org.gnome.baobab.desktop
+	sed -E -i s/'Name=Disks'/'Name=Disk Utility'/g /usr/share/applications/org.gnome.DiskUtility.desktop
 	sed -E -i s/'Name=Document Viewer'/'Name=Preview'/g /usr/share/applications/org.gnome.Evince.desktop
 	sed -E -i s/'Name=Empathy'/'Name=Messages'/g /usr/share/applications/empathy.desktop
 	sed -E -i s/'Name=Evolution'/'Mail=Mail'/g /usr/share/applications/org.gnome.Evolution.desktop
@@ -87,25 +87,26 @@ rename_applications() {
 	sed -E -i s/'Name=Visual Studio Code'/'Name=Code'/g /usr/share/applications/code.desktop
 	sed -E -i s/'Name=VMware Workstation'/'Name=Workstation'/g /usr/share/applications/vmware-workstation.desktop
 	sed -E -i s/'Name=Web'/'Name=Browser'/g /usr/share/applications/org.gnome.Epiphany.desktop
-};
+}
 
 icon_applications() {
 	echo 'Changing application icons...'
+	sed -E -i s/'^Icon=.*'/'Icon=\/usr\/share\/icons\/custom\/code.svg'/g /usr/share/applications/code.desktop
+	sed -E -i s/'^Icon=.*'/'Icon=\/usr\/share\/icons\/custom\/sublime-text.svg'/g /usr/share/applications/sublime_text.desktop
 	sed -E -i s/'^Icon=.*'/'Icon=cheese'/g /usr/share/applications/teams.desktop
 	sed -E -i s/'^Icon=.*'/'Icon=games-app'/g /usr/share/applications/org.gnome.Games.desktop
 	sed -E -i s/'^Icon=.*'/'Icon=gnome-books'/g /usr/share/applications/org.gnome.Books.desktop
 	sed -E -i s/'^Icon=.*'/'Icon=internet-chat'/g /usr/share/applications/org.gnome.Polari.desktop
+	sed -E -i s/'^Icon=.*'/'Icon=jockey'/g /usr/share/applications/vmware-player.desktop
 	sed -E -i s/'^Icon=.*'/'Icon=jockey'/g /usr/share/applications/vmware-workstation.desktop
 	sed -E -i s/'^Icon=.*'/'Icon=mail-app'/g /usr/share/applications/org.gnome.Geary.desktop
 	sed -E -i s/'^Icon=.*'/'Icon=messaging-app'/g /var/lib/snapd/desktop/applications/discord_discord.desktop
 	sed -E -i s/'^Icon=.*'/'Icon=messaging-app'/g /var/lib/snapd/desktop/applications/fractal_fractal.desktop
 	sed -E -i s/'^Icon=.*'/'Icon=snap-store'/g /var/lib/snapd/desktop/applications/snap-store_ubuntu-software.desktop
 	sed -E -i s/'^Icon=.*'/'Icon=software-store'/g /usr/share/applications/org.gnome.Software.desktop
-	sed -E -i s/'^Icon=.*'/'Icon=\/usr\/share\/icons\/custom\/code.svg'/g /usr/share/applications/code.desktop
-	sed -E -i s/'^Icon=.*'/'Icon=\/usr\/share\/icons\/custom\/sublime-text.svg'/g /usr/share/applications/sublime_text.desktop
 	sed -E -i s/'^Icon=.*'/'Icon=web-browser'/g /usr/share/applications/google-chrome.desktop
 	sed -E -i s/'^Icon=.*'/'Icon=web-browser'/g /var/lib/snapd/desktop/applications/chromium_chromium.desktop
-};
+}
 
 add_startup_wmclass() {
 	echo 'Changing window settings...'
@@ -113,140 +114,140 @@ add_startup_wmclass() {
 	sed -i '/^StartupNotify=.*/a StartupWMClass=vmplayer' /usr/share/applications/vmware-player.desktop
 	sed -i '/^StartupNotify=.*/a StartupWMClass=vmware-netcfg' /usr/share/applications/vmware-netcfg.desktop
 	sed -i '/^StartupNotify=.*/a StartupWMClass=vmware' /usr/share/applications/vmware-workstation.desktop
-};
+}
 
 edit_startup_wmclass() {
 	nano /usr/share/applications/teams.desktop
 	nano /usr/share/applications/vmware-netcfg.desktop
 	nano /usr/share/applications/vmware-player.desktop
 	nano /usr/share/applications/vmware-workstation.desktop
-};
+}
 
 edit_gsettings() {
 	echo 'Changing gnome theme...'
 	gsettings set org.gnome.desktop.interface gtk-theme 'Yaru-dark'
-};
+}
 
 require_system_restart() {
 	touch /var/run/reboot-required
 	echo && read -p "Do you want to restart? [Y/n] " user_input_selection
 	echo
 	case "$user_input_selection" in
-		[Yy] | [Yy][Ee][Ss])
-			shutdown --reboot now
-			;;
-		[Ss] | [Ss][Hh][Uu][Tt][Dd][Oo][Ww][Nn])
-			shutdown --poweroff +1 'Initiated by $(basename -s .sh $0)'
-			;;
-		*)
-			echo '*** System restart required ***' 1>&2
-			exit 0
-			;;
+	[Yy] | [Yy][Ee][Ss])
+		shutdown --reboot now
+		;;
+	[Ss] | [Ss][Hh][Uu][Tt][Dd][Oo][Ww][Nn])
+		shutdown --poweroff +1 'Initiated by $(basename -s .sh $0)'
+		;;
+	*)
+		echo '*** System restart required ***' 1>&2
+		exit 0
+		;;
 	esac
-};
+}
 
 display_help_message() {
-	cat <<- EOF_XYZ
-	Usage: $(basename -s .sh $0) [OPTION]...
-	Personalize Ubuntu with custom settings, icons, packages
+	cat <<-EOF_XYZ
+		Usage: $(basename -s .sh $0) [OPTION]...
+		Personalize Ubuntu with custom settings, icons, packages
 
-	The following options and long options are understood:
-	 -a, --all		all personalzation tasks
-	 -d, --devel		add 'StartupWMClass' to select applications
-	 -h, --hide		hide select applications
-	 -i, --install		install additional software packages
-	 -m, --modify		modify application settings using the default editor
-	            		  includs name, icon and gsettings
-	 -H, --help		display this help information
-	 -V, --version		display the version information
+		The following options and long options are understood:
+		 -a, --all      All personalzation tasks
+		 -d, --devel    Add 'StartupWMClass' to select applications
+		 -h, --hide     Hide select applications
+		 -i, --install  Install additional software packages
+		 -m, --modify   Modify application settings using the default
+		                  editor includs name, icon and gsettings
+		 -H, --help     Display this help information
+		 -V, --version  Display the version information
 
-	Copyright:
-	 Copyright (c) 2019-$(date +%Y) Robert LaRocca http://www.laroccx.com
-	 License: The MIT License (MIT)
-	 Source Code: https://github.com/robertlarocca/personalize-ubuntu-desktop
+		Copyright:
+		 Copyright (c) 2019-$(date +%Y) Robert LaRocca http://www.laroccx.com
+		 License: The MIT License (MIT)
+		 Source Code: https://github.com/robertlarocca/personalize-ubuntu-desktop
 	EOF_XYZ
-};
+}
 
 display_version_info() {
-	cat <<- EOF_XYZ
-	System: $(lsb_release -ds)
-	Desktop: $(gnome-shell --version)
-	Kernel: Linux $(uname -r)
-	Shell: Bash $BASH_VERSION
+	cat <<-EOF_XYZ
+		System: $(lsb_release -ds)
+		Desktop: $(gnome-shell --version)
+		Kernel: Linux $(uname -r)
+		Shell: Bash $BASH_VERSION
 
-	$(basename -s .sh $0) $script_version-$script_release
-	Copyright (c) 2019-$(date +%Y) Robert LaRocca http://www.laroccx.com
-	License: The MIT License (MIT)
-	Source Code: https://github.com/robertlarocca/personalize-ubuntu-desktop
+		$(basename -s .sh $0) $script_version-$script_release
+		Copyright (c) 2019-$(date +%Y) Robert LaRocca http://www.laroccx.com
+		License: The MIT License (MIT)
+		Source Code: https://github.com/robertlarocca/personalize-ubuntu-desktop
 	EOF_XYZ
-};
+}
 
 main() {
 	case "$1" in
-		-a | --all)
+	-a | --all)
+		require_root_privileges
+		install_packages
+		hide_applications
+		rename_applications 2>/dev/null
+		icon_applications 2>/dev/null
+		add_startup_wmclass
+		edit_startup_wmclass
+		edit_gsettings
+		require_system_restart
+		;;
+	-d | --devel)
+		require_root_privileges
+		add_startup_wmclass
+		edit_startup_wmclass
+		require_system_restart
+		;;
+	-D | --dpkg)
+		require_root_privileges
+		hide_applications
+		rename_applications 2>/dev/null
+		icon_applications 2>/dev/null
+		edit_gsettings
+		exit 0
+		;;
+	-h | --hide)
+		require_root_privileges
+		hide_applications
+		require_system_restart
+		;;
+	-i | --install)
+		require_root_privileges
+		install_packages
+		require_system_restart
+		;;
+	-m | --modify)
+		require_root_privileges
+		rename_applications 2>/dev/null
+		icon_applications 2>/dev/null
+		edit_gsettings
+		require_system_restart
+		;;
+	-H | --help)
+		display_help_message
+		exit 0
+		;;
+	-V | --version)
+		display_version_info
+		exit 0
+		;;
+	*)
+		if [ ! -z "$1" ]; then
+			echo "$(basename $0): invalid option '$1'"
+			echo "Type '$(basename $0) --help' for a list of available options."
+		else
 			require_root_privileges
-			install_packages
 			hide_applications
-			rename_applications 2> /dev/null
-			icon_applications 2> /dev/null
-			add_startup_wmclass
-			edit_startup_wmclass
+			rename_applications 2>/dev/null
+			icon_applications 2>/dev/null
 			edit_gsettings
 			require_system_restart
-			;;
-		-d | --devel)
-			require_root_privileges
-			add_startup_wmclass
-			edit_startup_wmclass
-			require_system_restart
-			;;
-		-D | --dpkg)
-			require_root_privileges
-			hide_applications
-			rename_applications 2> /dev/null
-			icon_applications 2> /dev/null
-			edit_gsettings
-			exit 0
-			;;
-		-h | --hide)
-			require_root_privileges
-			hide_applications
-			require_system_restart
-			;;
-		-i | --install)
-			require_root_privileges
-			install_packages
-			require_system_restart
-			;;
-		-m | --modify)
-			require_root_privileges
-			rename_applications 2> /dev/null
-			icon_applications 2> /dev/null
-			edit_gsettings
-			require_system_restart
-			;;
-		-H | --help)
-			display_help_message
-			exit 0
-			;;
-		-V | --version)
-			display_version_info
-			exit 0
-			;;
-		*)
-			if [ ! -z "$1" ]; then
-				echo "$(basename $0): invalid option '$1'"
-				echo "Type '$(basename $0) --help' for a list of available options."
-			else
-				require_root_privileges
-				hide_applications
-				rename_applications 2> /dev/null
-				icon_applications 2> /dev/null
-				edit_gsettings
-				require_system_restart
-			fi
-			;;
+		fi
+		;;
 	esac
-};
+}
 
 main "$1"
